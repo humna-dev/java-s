74260819dfb2c31b635cb13956ad14c1237a613c
humna-dev <hamnaallauddin4@gmail.com>
2023-04-06 14:45:57 -0700

    h1

---
+++ script/apps.js
@@ -0,0 +1,144 @@
+// 6 april- chapter 1 
+ //alert("I am an alert box"); 
+
+ // chapter 2
+ //var name = " Humna";
+  //name = "Ace";
+ //alert(name)
+
+ //Chapter 3
+ // var nationality = "France";
+// alert(nationality)
+
+// var floogle = "Humna";
+// alert(floogle)
+
+
+// var lessonAuthor = "Humna";
+// var guyWhoKeepsSayingHisOwnName = "humna";
+// var x = "Humna";
+// alert(x)
+
+ //var lastName = " humna"
+  //var cityOfOrigin = "mesopotamia";
+ //var aussieGreeting = "g'Day";
+//alert(aussieGreeting)
+
+//var thanx = "Thanks for your input!"
+ //alert (thanx);
+
+
+ //chapter 3
+ // var age =+ prompt("18")
+// var fage =+ prompt("50")
+// alert(age+fage)
+// alert(Number(age) + Number(fage))
+// console.log(typeof age)
+
+
+// let x =7
+// let y =4
+// let z =x+y
+// alert(z)
+ 
+ //var originalNumber = 23;
+ //var newNumber = originalNumber + 7
+// alert (newNumber)
+
+// var caseQty = 144
+// alert(caseQty)
+
+// chapter 4 no coding
+
+
+// chapter 5 
+
+// var popularNumber = 2 + 2
+// alert(2 + 2)
+
+// var popularNumber = 12 - 24
+// alert(12 - 24)
+
+// var popularNumber = 3 * 12;
+// alert(3 * 12)
+
+// var num = 10
+// var popularNumber = num + 200
+// alert(popularNumber)
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// alert(popularNumber)
+
+// var whatsLeftOver = 9 % 3;
+// alert(whatsLeftOver)
+
+
+// chapter 6 
+
+// let x = 8;
+// x++;
+// let z = x;
+// alert(z)
+
+// var num = 1;
+// var newNum = num++;
+// alert(newNum)
+
+//var num = 1
+//var newNum = ++num
+ //alert(newNum)
+
+// var num = 5;
+// var newNum = num--;
+// alert(newNum)
+
+// var num = 5;
+// var newNum = --num
+// alert(newNum)
+
+
+// chapter 7 
+//var totalCost = 1 + 3 * 4
+//alert(totalCost)
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// alert(resultOfComputation)
+
+// var resultOfComputation = (5 * 6) * 2 + 7
+// alert(resultOfComputation)
+
+
+// chapter 8
+
+// var message = "Your location plz";
+// alert(message);
+
+// var message = "Thanks,Humna";
+// var banger = "!";
+// alert(message + banger)
+
+// var message = "Thanks, "
+// var userName = "Humna"
+// var banger = "!"
+// var customMess = message + userName + banger
+// alert(customMess)
+
+// var 2 + 2 = "2 plus 2 equals "
+// alert( 2 + 2)
+
+// alert("2" + "2");
+
+
+// chapter 9 
+
+// var spec = prompt("Your name?", "humna") 
+
+// var question = "Your CNIC?";
+// var defaultAnswer = "00000-0000000//";
+// var spec = prompt(question, defaultAnswer);
+
+// var numberOfCats = prompt("How much Food?");
+// var tooManyCats = numberOfFood + 1;
+

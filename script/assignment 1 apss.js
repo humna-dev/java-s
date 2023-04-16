b1ce0e74fbf09623669ef3feaf51b0c87d7ce1b7
humna-dev <hamnaallauddin4@gmail.com>
2023-04-15 20:50:26 -0700

    FEAT : JS assignment

---
+++ script/assignment 1 apss.js
@@ -0,0 +1,116 @@
+// Assignment 1
+
+
+// Task 1 Calculate the zakat value
+
+
+//var zakatPercentage = 0.025;
+//var userInput = +prompt("input value");
+//var result = zakatPercentage * userInput;
+//alert("Your zakat value =  " + result);
+
+
+//Task 2 calculate the fitrah amount,
+
+//ar familyMembers =
+//prompt("Enter the total number of family members");
+
+//var fitrah= prompt("Choose a fitrah method ,jaw - Rs500 ,Wheat - Rs300 ,kishmish - Rs1200 ");
+//var price;
+//if (fitrah == 'jaw') {
+//price = 500;
+//} else if (fitrah == 'wheat') {
+//price = 300;
+//} else if (fitrah == 'kishmish') {
+//alert("Please choose a valid option");
+//}
+
+//var totalFitrah = price * familyMembers;
+
+//alert("your total fitrah = " + totalFitrah );
+
+
+// Task 3  Create a program that generates a random number between 1 and 10
+//var secretNumber = 10;
+
+//var guess = prompt("enter a guess for the secret number from 1 to 10");
+
+//if (guess == secretNumber) {
+//alert("Congratulations! You guessed the secret number.");
+//} else if (guess < secretNumber) {
+//alert("Too low guess again!");
+//} else {
+//alert("Too high! guess again");
+//}
+
+
+
+//Task 4 Capitalized Name
+
+//var name = prompt("Enter your name:");
+//var capitalized = name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
+//a//lert(capitalized);
+
+
+
+
+  
+// Task 5  Two empty array Cnumber , Cname;.
+ //var contactNumbers = [];
+ //var contactNames = [];
+ //for//( i=0 ; i<5 ; i++){
+    //var Number = +prompt ( " PLEASE ENTER NUMBER ");
+    //var Name = prompt (" PLEASE ENTER NAME")
+ 
+//contactNames.push(Number);
+   //contactNumbers.push(Name);}
+ 
+ //for ( i = 0; i < contactNumbers.length; i++) {
+  //console.log(contactNumbers[i] + " ; " + contactNames[i]);
+ //}
+
+
+ // Task 6 product remaining and remove 
+ 
+ //var products = ["lays", "popstar", "chilymily", "chetoos", "salanty"];
+
+//var position =+prompt("which product you want to buy (0-" + products.length + ")");
+
+//var removedproduct = products.splice(position - 0, 1);
+
+
+//console.log("Removed product: " + removedproduct);
+//console.log("Remaining products : " + products);
+//console.log("remaining products : " + products.length);
+
+//Task 7 nationality , gender ,age for vote 
+//var nationality = prompt("enter your nationality?");
+//if (nationality.toLowerCase() === "pakistani" || nationality.toLowerCase() === "indian") 
+  //var gender = prompt(" please enter your gender?");
+ // var age =+prompt(" please enter your age?");
+ // if (gender.toLowerCase() === "male") {
+   // if (age >= 18) {
+     // console.log("congratulation!  you are eligible to vote.");
+    //} else {
+    //  console.log("am sorry you are not eligible to vote..");
+   // }
+ // } else if (gender.toLowerCase() === "female") 
+    //if (age >= 18) {
+      //var married = prompt("arr you married? please select yes or no ");
+      //if (married.toLowerCase() === "yes") {
+        //console.log("you are eligible to vote mam.");
+      //} else {
+        //console.log("you are not eligible to vote mam");
+      //}
+    
+//}
+
+
+//Task 8 You have an array of that contains the name of Pakistani Teams Player selected for
+//WorldCup(15 Players)
+//var worldcupSquad = ["Babar Azam", "Imam-ul-Ha", "Fakhar Zaman", "Mohammad Rizwan", "Shoaib Mali", "Asif Ali", "Imad Wasim", "Shadab Khan", "Hasan Ali", "Shaheen Afridi", "Haris Rauf", "Sarfaraz Ahmed", "Mohammad Nawaz", "Mohammad Hasnain", "Usman Qadir"];
+
+//var finalTeam = worldcupSquad.slice(0, 11);
+
+//console.log(finalTeam);
+
